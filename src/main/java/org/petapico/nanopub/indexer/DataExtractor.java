f4e832f511f4d08886c36c4423b8375eeaa92fcf
Kishan <kishanjay@live.nl>
2015-11-06 16:47:13 +0100

    cleaned up code

--- src/main/java/org/petapico/nanopub/indexer/DataExtractor.java
+++ src/main/java/org/petapico/nanopub/indexer/DataExtractor.java
@@ -1,6 +1,7 @@
 package org.petapico.nanopub.indexer;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.nanopub.Nanopub;
 import org.nanopub.NanopubUtils;
@@ -22,33 +23,40 @@ public class DataExtractor {
 	}
 
 	public void run() throws IOException, RDFHandlerException {
+		//Server iterator goes through every server
 		ServerIterator serverIterator = new ServerIterator();
 		if (!serverIterator.hasNext()){
 			System.out.println("ERROR: NO SERVERS FOUND");
 			System.exit(0);
 		}
+		//for testing purposes we only use the first server
 		ServerInfo si = serverIterator.next();
 		System.out.println("==========");
 		System.out.println("Server: " + si.getPublicUrl() + "\n");
-		// This is just a test so we only check the first page of nanopubs:
+		
 		int page = 0;
-		int nanopubNumber = 0;
+		int currentNanopub = 0;
 		int totalNanopubs = (int) (si.getNextNanopubNo()-1);
-		while (nanopubNumber < totalNanopubs){ //There should be a page left
-			page += 1; //read from the next page
+		while (currentNanopub < totalNanopubs){ //There should be a page left
+			//read from the next page
+			page += 1; 
 			System.out.println("page: " + page);
-			for (String nanopubId : NanopubServerUtils.loadNanopubUriList(si, page)) {
-				String ac = org.nanopub.extra.server.GetNanopub.getArtifactCode(nanopubId); //GET IDENTIFIER
-				
-				System.out.println("Nanopub ID: " + ac);
-				
-				nanopubNumber++;
-				Nanopub np = GetNanopub.get(nanopubId);
-				//System.out.println("Nanopub content:\n" + NanopubUtils.writeToString(np, RDFFormat.TRIG) + "\n");
-				if (nanopubNumber > 3) {
-					// This is just a test, so we stop after 5 nanopubs...
+			List<String> nanopubsOnPage = NanopubServerUtils.loadNanopubUriList(si, page);
+			
+			if (nanopubsOnPage.size() == 0){
+				break; //There are no nanopubs on this page
+			}
+			
+			for (String nanopubId : nanopubsOnPage) {
+				if (1==1){
 					break;
 				}
+				String ac = org.nanopub.extra.server.GetNanopub.getArtifactCode(nanopubId); //GET IDENTIFIER
+				System.out.println("Nanopub ID: " + ac);
+				
+				currentNanopub++;
+				Nanopub np = GetNanopub.get(nanopubId);
+				//System.out.println("Nanopub content:\n" + NanopubUtils.writeToString(np, RDFFormat.TRIG) + "\n");
 			}
 		}
 		System.out.println("all pages visited: "+ page);
