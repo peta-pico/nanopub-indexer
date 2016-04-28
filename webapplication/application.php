a205841cf70dd470167aa721e2da251de572889a
Kishan <kishanjay@live.nl>
2016-04-28 21:58:14 +0200

    bug fix

--- webapplication/application.php
+++ webapplication/application.php
@@ -14,7 +14,7 @@ $searchuriarray = explode("\n", $searchuri);
 $basepath = "http://" . $_SERVER['HTTP_HOST'] . pathinfo($_SERVER["PHP_SELF"])['dirname'];
 
 $uri = $searchuriarray[0];
-$result = json_decode(trim(file_get_contents($basepath . '/database/api.php?format=json&search-uri=' . $uri)), true);
+$result = json_decode(trim(file_get_contents($basepath . '/database/api.php?format=json&search-uri=' . trim($uri))), true);
 foreach ($searchuriarray as $uri){
 	$url = $basepath . '/database/api.php?format=json&search-uri=' . trim($uri);
 	$tempresult = json_decode(trim(file_get_contents($url)), true);
