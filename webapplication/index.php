b3ac00760a4565f07dfa8515ea3935a8f2e13d43
Tobias Kuhn <kuhntobias@gmail.com>
2017-02-24 10:49:38 +0100

    Work on HTML table

--- webapplication/index.php
+++ webapplication/index.php
@@ -9,22 +9,22 @@
 <h1>Nanopublication Search</h1>
 
 <p>
-URIs (one URI per line): <br/><textarea rows="10" cols="100" name="search-uri" /></textarea><br />
-Page (0 for no pages): <input type="number" value="1" min="0" max="10000000" name="page"/><br/><br/>
+URIs (one URI per line): <br/><textarea rows="10" cols="100" name="search-uri" /></textarea><br/>
 <table>
+<tr><td>Page (0 for no pages):</td><td><input type="number" value="1" min="0" max="10000000" name="page"/></td></tr>
 <tr><td>Begin timestamp:</td><td><input type="number" name="begin_timestamp"/></td></tr>
 <tr><td>End timestamp:</td><td><input type="number" name="end_timestamp"/></td></tr>
+<tr><td>Order:</td><td><input type="radio" name="order" value="0"/>unordered
+  <input type="radio" name="order" value="1" checked/>descending by timestamp
+  <input type="radio" name="order" value="2"/>ascending by timestamp</td></tr>
+<tr><td>Graphs:</td><td><input type="checkbox" name="head" checked />Head
+  <input type="checkbox" name="assertion" checked />Assertion
+  <input type="checkbox" name="provenance" checked />Provenance
+  <input type="checkbox" name="pubinfo" checked />Pubinfo</td></tr>
+<tr><td>Return format:</td><td><input type="radio" name="format" value="text" required checked />text
+  <input type="radio" name="format" value="json" required />JSON
+  <input type="radio" name="format" value="link" required />link</td></tr>
 </table>
-Order: <input type="radio" name="order" value="0"/>unordered
-<input type="radio" name="order" value="1" checked/>descending by timestamp
-<input type="radio" name="order" value="2"/>ascending by timestamp<br/>
-<input type="checkbox" name="head" checked />Head<br />
-<input type="checkbox" name="assertion" checked />Assertion<br />
-<input type="checkbox" name="provenance" checked />Provenance<br />
-<input type="checkbox" name="pubinfo" checked />Pubinfo<br />
-<input type="radio" name="format" value="text" required checked />text<br />
-<input type="radio" name="format" value="json" required />JSON<br />
-<input type="radio" name="format" value="link" required />link<br />
 </p>
 
 <input type="submit" value="Search" />
