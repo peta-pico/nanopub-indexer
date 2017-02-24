19cf0302074cb56dc1d6f8df7041902a57f80e22
Tobias Kuhn <kuhntobias@gmail.com>
2017-02-24 10:28:52 +0100

    Experimenting with HTML table...

--- webapplication/index.php
+++ webapplication/index.php
@@ -5,30 +5,29 @@
 </head>
 <body>
 <form method="GET" action="database/api.php">
-	<h1>Nanopublication Search</h1>
-	<p>
-		URIs (one URI per line): <br/><textarea rows="10" cols="100" name="search-uri" /></textarea><br />
-		Page (0 for no pages): <input type="number" value="1" min="0" max="10000000" name="page"/><br/><br/>
-		Begin timestamp: <input type="number" name="begin_timestamp"/><br/>
-		End timestamp: <input type="number" name="end_timestamp"/><br/>
-		Order: <input type="radio" name="order" value="0"/>unordered
-		<input type="radio" name="order" value="1" checked/>descending by timestamp
-		<input type="radio" name="order" value="2"/>ascending by timestamp<br/>
-		
-		<input type="checkbox" name="head" checked />Head<br />
-		<input type="checkbox" name="assertion" checked />Assertion<br />
-		<input type="checkbox" name="provenance" checked />Provenance<br />
-		<input type="checkbox" name="pubinfo" checked />Pubinfo<br />
-		
-		<input type="radio" name="format" value="text" required checked />text<br />
-		<input type="radio" name="format" value="json" required />JSON<br />
-		<input type="radio" name="format" value="link" required />link<br />
 
+<h1>Nanopublication Search</h1>
 
-		
-	</p>
+<p>
+URIs (one URI per line): <br/><textarea rows="10" cols="100" name="search-uri" /></textarea><br />
+Page (0 for no pages): <input type="number" value="1" min="0" max="10000000" name="page"/><br/><br/>
+<table>
+<tr><th>Begin timestamp:</th><th><input type="number" name="begin_timestamp"/></th></tr>
+<tr><th>End timestamp:</th><th><input type="number" name="end_timestamp"/></th></tr>
+</table>
+Order: <input type="radio" name="order" value="0"/>unordered
+<input type="radio" name="order" value="1" checked/>descending by timestamp
+<input type="radio" name="order" value="2"/>ascending by timestamp<br/>
+<input type="checkbox" name="head" checked />Head<br />
+<input type="checkbox" name="assertion" checked />Assertion<br />
+<input type="checkbox" name="provenance" checked />Provenance<br />
+<input type="checkbox" name="pubinfo" checked />Pubinfo<br />
+<input type="radio" name="format" value="text" required checked />text<br />
+<input type="radio" name="format" value="json" required />JSON<br />
+<input type="radio" name="format" value="link" required />link<br />
+</p>
 
-	<input type="submit" value="Search" />
+<input type="submit" value="Search" />
 </form>	
 <hr />
 <h3><a href="indexes.php">Indexes</a></h3>
